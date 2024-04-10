60ec8cd6536868d95036c0f1a10a28fa43bdc304
alex <alex@pdp7.net>
2024-04-10 15:49:42 +0200

    Add another example

---
+++ examples/quieter.rs
@@ -0,0 +1,23 @@
+use rayon::prelude::*;
+use tracing::instrument;
+
+use ductape;
+
+#[instrument]
+pub fn do_stuff(arg: &str) {
+    ductape::run(&format!("thread {arg} start"), &["echo", "first"]);
+    for i in 0..arg.parse().unwrap() {
+        ductape::run(&format!("thread {arg} sleep {i}"), &["sleep", arg]);
+        ductape::run(&format!("thread {arg} print {i}"), &["echo", arg]);
+    }
+    ductape::run(&format!("thread {arg} end"), &["echo", "last"]);
+}
+
+pub fn main() {
+    ductape::setup();
+    std::env::args()
+        .skip(1)
+        .collect::<Vec<_>>()
+        .par_iter()
+        .for_each(|arg| do_stuff(arg));
+}
