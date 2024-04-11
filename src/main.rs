621c89e428d7743effc093e07eb7185b693e8f88
alex <alex@pdp7.net>
2024-04-11 19:00:04 +0200

    Add a main that wraps a command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
-    println!("Hello, world!");
+use tracing_subscriber::filter::LevelFilter;
+
+pub fn main() {
+    ductape::setup(LevelFilter::INFO);
+    let args = std::env::args().skip(1).collect::<Vec<_>>();
+    let args = args.iter().map(|s| s.as_str()).collect::<Vec<_>>();
+    ductape::run(args[0], args.as_slice());
+    std::thread::sleep(std::time::Duration::from_secs(1));
 }
